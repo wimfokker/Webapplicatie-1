cace3cf62505cf7d694b50323004164feecbb7f0
DoakesdextermothaF <1217945@student.roc-nijmegen.nl>
2026-03-12 15:18:08 +0100

    EoD
    I edited the file change for index.html to index.php. And theres also added a database.php were the webserver now runs. On the index.php is a include_once 'database' that has problem that needs to be resolved.

---
+++ app/database.php
@@ -0,0 +1,31 @@
+<?php 
+
+    $host = 'db';
+    $db = 'mydatabase';
+    $user = 'user';
+    $password = 'password';
+    $charset = 'utf8mb4';
+
+    // pdo opties
+
+    $opties = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    // dsn
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try {
+        //Create the connnection
+        $pdo = new PDO($dsn, $user, $password, $opties);
+        //succes melding
+        echo "Databse connection gelukt <br/>";
+    }   catch (PDOException $e) {
+        //foutmelding
+        echo $e->getMessage();
+        // stop (die)
+        die("Sorry, database probleem");
+    }
+?>
